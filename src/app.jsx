da258a91bf499e73ab851fd1ab93a1d61a61f799
mdramirez071 <mdramirez071@gmail.com>
2019-06-30 10:23:40 -0700

    adds final project files

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
-
+import TopSpot from './topspot';
+const axios = require('axios');
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        topspots: []
+    }
+}
+
+componentWillMount() {
+  axios
+    .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+    .then(response => response.data)
+    .then(topspots => this.setState({ topspots }));
+    console.log(this.state.topspots); //prints out the JSON file that is being grabbed in the GET request
+}
+
   render() {
+    
     return (
-      <div className='App'></div>
+      <div className='App container'>
+          <div className="jumbotron jumbotron-fluid">
+            <h4 className="display-4">San Diego Top Spots</h4>
+            <hr className="my-4"></hr>
+            <p>A list of the top 30 places to visit in San Diego, California.</p>
+            <p className="lead">
+              <a className="btn btn-primary btn-lg" onClick={() => this.componentWillMount()} href="#" role="button">Sample Button</a>
+            </p>
+            </div>
+            {/* <div className= "well well-lg"><pre>{ JSON.stringify(this.state.topspots, null, 2) }</pre></div> */}
+            { 
+      this.state.topspots.map(topspot => (
+          <TopSpot
+              key={topspot.id}
+              name={topspot.name}
+              description={topspot.description}
+              location={topspot.location} />
+      ))
+  }
+          </div>
     );
+   
   }
 }
 
---
+++ src/topspot.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props => (
+<div className='well'>
+    <h4>{props.name}</h4>
+    <p>{props.description}</p>
+    <a className="btn btn-primary btn-lg"  href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`} role='button'>Click This Link</a>
+</div>
+);
